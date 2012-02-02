ad0a5b5f93c0b36bb6bca88d633093fc3760c83b
Andreas Kupries <akupries@shaw.ca>
2012-02-02 06:45:03 +0000

    Implemented the gesture callback binding

--- kt_gesture.tcl
+++ kt_gesture.tcl
@@ -15,6 +15,86 @@ critcl::class def kinetcl::Gesture {
 
     # # ## ### ##### ######## #############
     # # ## ### ##### ######## #############
+
+    ::kt_2callback gesture \
+	xnRegisterGestureCallbacks \
+	xnUnregisterGestureCallbacks \
+	gestureRecognized {
+	    {const XnChar*    strGesture}
+	    {const XnPoint3D* pIDPosition}
+	    {const XnPoint3D* pEndPosition}
+	} {
+	    Tcl_Obj* coord [3];
+
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewStringObj (strGesture,-1));
+
+	    coord [0] = Tcl_NewDoubleObj (pIDPosition->X);
+	    coord [1] = Tcl_NewDoubleObj (pIDPosition->Y);
+	    coord [2] = Tcl_NewDoubleObj (pIDPosition->Z);
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewListObj (3,coord));
+
+	    coord [0] = Tcl_NewDoubleObj (pEndPosition->X);
+	    coord [1] = Tcl_NewDoubleObj (pEndPosition->Y);
+	    coord [2] = Tcl_NewDoubleObj (pEndPosition->Z);
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewListObj (3,coord));
+	} \
+	gestureProgress {
+	    {const XnChar*    strGesture}
+	    {const XnPoint3D* pPosition}
+	    {XnFloat          fProgress}
+	} {
+	    Tcl_Obj* coord [3];
+
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewStringObj (strGesture,-1));
+
+	    coord [0] = Tcl_NewDoubleObj (pPosition->X);
+	    coord [1] = Tcl_NewDoubleObj (pPosition->Y);
+	    coord [2] = Tcl_NewDoubleObj (pPosition->Z);
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewListObj (3,coord));
+
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewDoubleObj (fProgress));
+	}
+
+    ::kt_callback gestureChange \
+	xnRegisterToGestureChange \
+	xnUnregisterFromGestureChange \
+	{} {}
+
+    ::kt_callback gestureStageComplete \
+	xnRegisterToGestureIntermediateStageCompleted \
+	xnUnregisterFromGestureIntermediateStageCompleted \
+	{
+	    {const XnChar*    strGesture}
+	    {const XnPoint3D* pPosition}
+	} {
+	    Tcl_Obj* coord [3];
+
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewStringObj (strGesture,-1));
+
+	    coord [0] = Tcl_NewDoubleObj (pPosition->X);
+	    coord [1] = Tcl_NewDoubleObj (pPosition->Y);
+	    coord [2] = Tcl_NewDoubleObj (pPosition->Z);
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewListObj (3,coord));
+	}
+
+    ::kt_callback gestureStageReadyForNext \
+	xnRegisterToGestureReadyForNextIntermediateStage \
+	xnUnregisterFromGestureReadyForNextIntermediateStage \
+	{
+	    {const XnChar*    strGesture}
+	    {const XnPoint3D* pPosition}
+	} {
+	    Tcl_Obj* coord [3];
+
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewStringObj (strGesture,-1));
+
+	    coord [0] = Tcl_NewDoubleObj (pPosition->X);
+	    coord [1] = Tcl_NewDoubleObj (pPosition->Y);
+	    coord [2] = Tcl_NewDoubleObj (pPosition->Z);
+	    Tcl_ListObjAppendElement (interp, cmd, Tcl_NewListObj (3,coord));
+	}
+
+    # # ## ### ##### ######## #############
 }
 
 # # ## ### ##### ######## #############
