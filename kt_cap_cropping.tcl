e7ded27113f74f277b586b55d7298f4e908eb9b2
Andreas Kupries <akupries@shaw.ca>
2012-03-07 05:04:41 +0000

    Implemented cropping capability.

--- kt_cap_cropping.tcl
+++ kt_cap_cropping.tcl
@@ -5,30 +5,88 @@ critcl::class def ::kinetcl::CapCropping {
     # # ## ### ##### ######## #############
     ::kt_abstract_class
 
-    # XXX TODO Implement the API.
+    # # ## ### ##### ######## #############
+
+    mdef crop { /* Syntax: <instance> crop ?x y w h? */
+	XnStatus s;
+	XnCropping c;
+
+	if (objc == 2) { /* Syntax: <instance> crop */
+	    Tcl_Obj* cv[5];
+
+	    s = xnGetCropping (instance->handle, &c);
+	    CHECK_STATUS_RETURN;
+
+	    cv [0] = Tcl_NewIntObj (c.bEnabled);
+	    cv [1] = Tcl_NewIntObj (c.nXOffset);
+	    cv [2] = Tcl_NewIntObj (c.nYOffset);
+	    cv [3] = Tcl_NewIntObj (c.nXSize);
+	    cv [4] = Tcl_NewIntObj (c.nYSize);
+
+	    Tcl_SetObjResult (interp, Tcl_NewListObj (5, cv));
+	    return TCL_OK;
+	}
+
+	if (objc == 6) { /* Syntax: <instance> crop x y w h */
+	    int i;
+
+	    if (Tcl_GetIntFromObj (interp, objv [2], &i) != TCL_OK) {
+		return TCL_ERROR;
+	    }
+	    c.nXOffset = i;
+	    if (Tcl_GetIntFromObj (interp, objv [3], &i) != TCL_OK) {
+		return TCL_ERROR;
+	    }
+	    c.nYOffset = i;
+	    if (Tcl_GetIntFromObj (interp, objv [4], &i) != TCL_OK) {
+		return TCL_ERROR;
+	    }
+	    c.nXSize = i;
+	    if (Tcl_GetIntFromObj (interp, objv [5], &i) != TCL_OK) {
+		return TCL_ERROR;
+	    }
+	    c.nYSize = i;
+	    c.bEnabled = 1;
+
+	    s = xnSetCropping (instance->handle, &c);
+	    CHECK_STATUS_RETURN;
+
+	    return TCL_OK;
+	}
+
+	Tcl_WrongNumArgs (interp, 2, objv, "?x y w h?");
+	return TCL_ERROR;
+    }
+
+    mdef uncrop { /* Syntax: <instance> uncrop */
+	XnStatus s;
+	XnCropping c;
+
+	if (objc != 2) { /* Syntax: <instance> crop */
+	    Tcl_WrongNumArgs (interp, 2, objv, NULL);
+	    return TCL_ERROR;
+	}
+
+	c.bEnabled = 0;
+	c.nXOffset = 0;
+	c.nYOffset = 0;
+	c.nXSize = 0;
+	c.nYSize = 0;
+
+	s = xnSetCropping (instance->handle, &c);
+	CHECK_STATUS_RETURN;
+
+	return TCL_OK;
+    }
 
     # # ## ### ##### ######## #############
 
-    # # ## ### ##### ######## #############
-
-    # # ## ### ##### ######## #############
+    kt_callback cropping \
+	xnRegisterToCroppingChange \
+	xnUnregisterFromCroppingChange \
+    {} {}
 
     # # ## ### ##### ######## #############
 }
 
 # # ## ### ##### ######## #############
-if 0 {
-    XnCropping {
-	XnBool 	bEnabled
-	XnUInt16 	nXOffset
-	XnUInt16 	nYOffset
-	XnUInt16 	nXSize
-	XnUInt16 	nYSize
-    }
-
-XnStatus xnSetCropping (XnNodeHandle hInstance, const XnCropping *pCropping)
-XnStatus xnGetCropping (XnNodeHandle hInstance,       XnCropping *pCropping)
-
-XnStatus xnRegisterToCroppingChange (XnNodeHandle hInstance, XnStateChangedHandler handler, void *pCookie, XnCallbackHandle *phCallback)
-void 	xnUnregisterFromCroppingChange (XnNodeHandle hInstance, XnCallbackHandle hCallback)
-}
