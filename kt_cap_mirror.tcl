cd805405c693af480bb126ef7deb47d67af1af44
Andreas Kupries <akupries@shaw.ca>
2012-03-07 04:58:54 +0000

    Implementing mirroring capability.

--- kt_cap_mirror.tcl
+++ kt_cap_mirror.tcl
@@ -5,21 +5,49 @@ critcl::class def ::kinetcl::CapMirror {
     # # ## ### ##### ######## #############
     ::kt_abstract_class
 
-    # XXX TODO Implement the API.
+    # # ## ### ##### ######## #############
+
+    mdef mirror { /* Syntax: <instance> mirror <bool> */
+	int m;
+	XnStatus s;
+
+	if (objc != 3) {
+	    Tcl_WrongNumArgs (interp, 2, objv, "bool");
+	    return TCL_ERROR;
+	}
+
+	if (Tcl_GetBooleanFromObj (interp, objv[2], &m) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	s = xnSetMirror (instance->handle, m);
+	CHECK_STATUS_RETURN;
+
+	return TCL_OK;
+    }
+
+    mdef mirrored { /* Syntax: <instance> mirrored */
+	XnBool m;
+
+	if (objc != 2) {
+	    Tcl_WrongNumArgs (interp, 2, objv, NULL);
+	    return TCL_ERROR;
+	}
+
+	m = xnIsMirrored (instance->handle);
+
+	Tcl_SetObjResult (interp, Tcl_NewIntObj (m));
+	return TCL_OK;
+    }
 
     # # ## ### ##### ######## #############
 
-    # # ## ### ##### ######## #############
-
-    # # ## ### ##### ######## #############
+    kt_callback mirror \
+	xnRegisterToMirrorChange \
+	xnUnregisterFromMirrorChange \
+    {} {}
 
     # # ## ### ##### ######## #############
 }
 
 # # ## ### ##### ######## #############
-if 0 {
-XN_C_API XnStatus XN_C_DECL 	xnSetMirror (XnNodeHandle hInstance, XnBool bMirror)
-XN_C_API XnBool XN_C_DECL 	xnIsMirrored (XnNodeHandle hInstance)
-XN_C_API XnStatus XN_C_DECL 	xnRegisterToMirrorChange (XnNodeHandle hInstance, XnStateChangedHandler handler, void *pCookie, XnCallbackHandle *phCallback)
-XN_C_API void XN_C_DECL 	xnUnregisterFromMirrorChange (XnNodeHandle hInstance, XnCallbackHandle hCallback)
-}
