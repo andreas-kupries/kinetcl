086b2b453a4c34f9163d7c7eaf49cf87b5d9828c
Andreas Kupries <akupries@shaw.ca>
2012-03-07 05:26:35 +0000

    Implemented anti-flicker capability

--- kt_cap_antiflicker.tcl
+++ kt_cap_antiflicker.tcl
@@ -5,30 +5,62 @@ critcl::class def ::kinetcl::CapAntiflicker {
     # # ## ### ##### ######## #############
     ::kt_abstract_class
 
-    # XXX TODO Implement the API.
+    # # ## ### ##### ######## #############
+
+    mdef frequency { /* Syntax: <instance> frequency ?f? */
+	if (objc == 2) { /* Syntax: <instance> frequency */
+	    Tcl_Obj* res;
+	    XnPowerLineFrequency f = xnGetPowerLineFrequency (instance->handle);
+
+	    if (f == ((XnPowerLineFrequency) -1)) {
+		Tcl_AppendResult (interp, "Antiflicker not supported", NULL);
+		return TCL_ERROR;
+	    }
+
+	    if (!f) {
+		res = Tcl_NewStringObj ("off",-1);
+	    } else {
+		res = Tcl_NewIntObj (f);
+	    }
+
+	    Tcl_SetObjResult (interp, res);
+	    return TCL_OK;
+	}
+
+	if (objc == 3) { /* Syntax: <instance> frequency off|n */
+	    XnStatus s;
+	    int f;
+
+	    if (0 == strcmp ("off", Tcl_GetString (objv [2]))) {
+		f = 0;
+	    } else if (Tcl_GetIntFromObj (interp, objv [2], &f) != TCL_OK) {
+		return TCL_ERROR;
+	    }
+	    if (f && (f != 50) && (f != 60)) {
+		char buf [20];
+		sprintf (buf, "%d", f);
+		Tcl_AppendResult (interp, "Bad frequency ", buf, ", expected 50, 60, or off", NULL);
+		return TCL_ERROR;
+	    }
+
+	    s = xnSetPowerLineFrequency (instance->handle, f);
+	    CHECK_STATUS_RETURN;
+
+	    return TCL_OK;
+	}
+
+	Tcl_WrongNumArgs (interp, 2, objv, "?frequency|off?");
+	return TCL_ERROR;
+    }
 
     # # ## ### ##### ######## #############
 
-    # # ## ### ##### ######## #############
-
-    # # ## ### ##### ######## #############
+    kt_callback powerlinefrequency \
+	xnRegisterToPowerLineFrequencyChange \
+	xnUnregisterFromPowerLineFrequencyChange \
+    {} {}
 
     # # ## ### ##### ######## #############
 }
 
 # # ## ### ##### ######## #############
-if 0 {
-enum XnPowerLineFrequency
-
-Enumerator:
-    XN_POWER_LINE_FREQUENCY_OFF 	
-    XN_POWER_LINE_FREQUENCY_50_HZ 	
-    XN_POWER_LINE_FREQUENCY_60_HZ
-
-XnStatus	     xnSetPowerLineFrequency (XnNodeHandle hGenerator, XnPowerLineFrequency nFrequency)
-XnPowerLineFrequency xnGetPowerLineFrequency (XnNodeHandle hGenerator)
-
-XnStatus xnRegisterToPowerLineFrequencyChange (XnNodeHandle hGenerator, XnStateChangedHandler handler, void *pCookie, XnCallbackHandle *phCallback)
-void 	 xnUnregisterFromPowerLineFrequencyChange (XnNodeHandle hGenerator, XnCallbackHandle hCallback)
-
-}
