3633f2e00d5568eb2bf9e0936e2242b4de5c5c61
Andreas Kupries <akupries@shaw.ca>
2012-03-07 05:38:23 +0000

    Implemented error-state capability

--- kt_cap_errorstate.tcl
+++ kt_cap_errorstate.tcl
@@ -5,13 +5,27 @@ critcl::class def ::kinetcl::CapErrorState {
     # # ## ### ##### ######## #############
     ::kt_abstract_class
 
-    # XXX TODO Implement the API.
+    # # ## ### ##### ######## #############
+
+    mdef error-state { /* Syntax: <instance> error-state */
+	XnStatus s;
+
+	if (objc != 2) {
+	    Tcl_WrongNumArgs (interp, 2, objv, NULL);
+	    return TCL_ERROR;
+	}
+
+	s = xnGetNodeErrorState (instance->handle);
+	Tcl_AppendResult (interp, xnGetStatusString (s), NULL);
+	return TCL_OK;
+    }
 
     # # ## ### ##### ######## #############
 
-    # # ## ### ##### ######## #############
-
-    # # ## ### ##### ######## #############
+    kt_callback errorstate \
+	xnRegisterToErrorStateChange \
+	xnUnregisterFromErrorStateChange \
+    {} {}
 
     # # ## ### ##### ######## #############
 }
