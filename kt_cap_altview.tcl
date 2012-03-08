79eba934b28b7a4c314e0faafd6082fb20ed13fe
Andreas Kupries <akupries@shaw.ca>
2012-03-08 06:14:38 +0000

    Completed implementation of alt.view and frame.sync capabilities, now that we can validate kinetcl instances.

--- kt_cap_altview.tcl
+++ kt_cap_altview.tcl
@@ -5,11 +5,76 @@ critcl::class def ::kinetcl::CapAlternativeViewpoint {
     # # ## ### ##### ######## #############
     ::kt_abstract_class
 
-    # XXX TODO Implement the API.
-
     # # ## ### ##### ######## #############
 
-    # # ## ### ##### ######## #############
+    mdef supports-view { /* Syntax: <instance> supports-view <other-node> */
+	XnNodeHandle other;
+	XnBool supported;
+
+	if (objc != 3) {
+	    Tcl_WrongNumArgs (interp, 2, objv, "node");
+	    return TCL_ERROR;
+	}
+
+	if (kinetcl_validate (interp, objv [2], &other) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	supported = xnIsViewPointSupported (instance->handle, other);
+
+	Tcl_SetObjResult (interp, Tcl_NewIntObj (supported));
+	return TCL_OK;
+    }
+
+    mdef set-view { /* Syntax: <instance> set-view <other-node> */
+	XnStatus s;
+	XnNodeHandle other;
+
+	if (objc != 3) {
+	    Tcl_WrongNumArgs (interp, 2, objv, "node");
+	    return TCL_ERROR;
+	}
+
+	if (kinetcl_validate (interp, objv [2], &other) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	s = xnSetViewPoint (instance->handle, other);
+	CHECK_STATUS_RETURN;
+	return TCL_OK;
+    }
+
+    mdef reset-view { /* Syntax: <instance> reset-view */
+	XnStatus s;
+
+	if (objc != 2) {
+	    Tcl_WrongNumArgs (interp, 2, objv, NULL);
+	    return TCL_ERROR;
+	}
+
+	s = xnResetViewPoint (instance->handle);
+	CHECK_STATUS_RETURN;
+	return TCL_OK;
+    }
+
+    mdef using-view { /* Syntax: <instance> using-view <other-node> */
+	XnNodeHandle other;
+	XnBool as;
+
+	if (objc != 3) {
+	    Tcl_WrongNumArgs (interp, 2, objv, "node");
+	    return TCL_ERROR;
+	}
+
+	if (kinetcl_validate (interp, objv [2], &other) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	as = xnIsViewPointAs (instance->handle, other);
+
+	Tcl_SetObjResult (interp, Tcl_NewIntObj (as));
+	return TCL_OK;
+    }
 
     # # ## ### ##### ######## #############
 
@@ -22,15 +87,4 @@ critcl::class def ::kinetcl::CapAlternativeViewpoint {
 }
 
 # # ## ### ##### ######## #############
-if 0 {
-    # Support requires ability to determine if a Tcl_Obj* holds a node
-    # object command (name), and to convert from that to an instnce
-    # handle.
-
-    # See framesync as well.
-
-    XnBool   xnIsViewPointSupported (XnNodeHandle hInstance, XnNodeHandle hOther)
-    XnStatus xnSetViewPoint         (XnNodeHandle hInstance, XnNodeHandle hOther)
-    XnStatus xnResetViewPoint       (XnNodeHandle hInstance)
-    XnBool   xnIsViewPointAs        (XnNodeHandle hInstance, XnNodeHandle hOther)
-}
+return
--- kt_cap_framesync.tcl
+++ kt_cap_framesync.tcl
@@ -5,11 +5,81 @@ critcl::class def ::kinetcl::CapFramesync {
     # # ## ### ##### ######## #############
     ::kt_abstract_class
 
-    # XXX TODO Implement the API.
-
     # # ## ### ##### ######## #############
 
-    # # ## ### ##### ######## #############
+    mdef can-sync-with { /* Syntax: <instance> can-sync <other-node> */
+	XnNodeHandle other;
+	XnBool supported;
+
+	if (objc != 3) {
+	    Tcl_WrongNumArgs (interp, 2, objv, "node");
+	    return TCL_ERROR;
+	}
+
+	if (kinetcl_validate (interp, objv [2], &other) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	supported = xnCanFrameSyncWith (instance->handle, other);
+
+	Tcl_SetObjResult (interp, Tcl_NewIntObj (supported));
+	return TCL_OK;
+    }
+
+    mdef start-sync-with { /* Syntax: <instance> start-sync-with <other-node> */
+	XnStatus s;
+	XnNodeHandle other;
+
+	if (objc != 3) {
+	    Tcl_WrongNumArgs (interp, 2, objv, "node");
+	    return TCL_ERROR;
+	}
+
+	if (kinetcl_validate (interp, objv [2], &other) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	s = xnFrameSyncWith (instance->handle, other);
+	CHECK_STATUS_RETURN;
+	return TCL_OK;
+    }
+
+    mdef stop-sync-with { /* Syntax: <instance> stop-sync-with <other-node> */
+	XnStatus s;
+	XnNodeHandle other;
+
+	if (objc != 3) {
+	    Tcl_WrongNumArgs (interp, 2, objv, "node");
+	    return TCL_ERROR;
+	}
+
+	if (kinetcl_validate (interp, objv [2], &other) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	s = xnStopFrameSyncWith (instance->handle, other);
+	CHECK_STATUS_RETURN;
+	return TCL_OK;
+    }
+
+    mdef synced-with { /* Syntax: <instance> synced-with <other-node> */
+	XnNodeHandle other;
+	XnBool as;
+
+	if (objc != 3) {
+	    Tcl_WrongNumArgs (interp, 2, objv, "node");
+	    return TCL_ERROR;
+	}
+
+	if (kinetcl_validate (interp, objv [2], &other) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	as = xnIsFrameSyncedWith (instance->handle, other);
+
+	Tcl_SetObjResult (interp, Tcl_NewIntObj (as));
+	return TCL_OK;
+    }
 
     # # ## ### ##### ######## #############
 
@@ -22,15 +92,4 @@ critcl::class def ::kinetcl::CapFramesync {
 }
 
 # # ## ### ##### ######## #############
-if 0 {
-    # Support requires ability to determine if a Tcl_Obj* holds a node
-    # object command (name), and to convert from that to an instnce
-    # handle.
-
-    # See alt.view as well.
-
-    XnBool   xnCanFrameSyncWith (XnNodeHandle hInstance, XnNodeHandle hOther)
-    XnStatus xnFrameSyncWith (XnNodeHandle hInstance, XnNodeHandle hOther)
-    XnStatus xnStopFrameSyncWith (XnNodeHandle hInstance, XnNodeHandle hOther)
-    XnBool   xnIsFrameSyncedWith (XnNodeHandle hInstance, XnNodeHandle hOther)
-}
+return
