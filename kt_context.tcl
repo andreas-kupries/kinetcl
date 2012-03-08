8e3b293fa80502f846214730a4343d23e257b7c9
Andreas Kupries <akupries@shaw.ca>
2012-03-08 05:08:04 +0000

    implemented various global commands, especially the global wait/update functionality. The wait-one-update-all makes use of the new instance validation.

--- kt_context.tcl
+++ kt_context.tcl
@@ -7,9 +7,13 @@
 ## structure which is initialized at class and instance construction
 ## time, via kinetcl_context ().
 
+# # ## ### ##### ######## #############
+
 critcl::buildrequirement {
     package require critcl::iassoc
 }
+
+# # ## ### ##### ######## #############
 
 critcl::ccode {
     #include <XnOpenNI.h>
@@ -26,6 +30,8 @@ critcl::ccode {
 	    goto error; \
 	}
 }
+
+# # ## ### ##### ######## #############
 
 critcl::iassoc::def kinetcl_context {} {
     /* Our OpenNI context
@@ -47,3 +53,126 @@ critcl::iassoc::def kinetcl_context {} {
 } {
     xnContextRelease (data->context);
 }
+
+# # ## ### ##### ######## #############
+## Validation at C-level. Calls out to Tcl, where this was much easier
+## to implement.  Automatically cleans up the stash through which the
+## handle is communicated to us. Nothing the caller has to bother
+## with.
+
+critcl::ccode {
+    static int
+    kinetcl_validate (Tcl_Interp* interp, Tcl_Obj* o, XnNodeHandle* handle)
+    {
+	kinetcl_context_data c;
+	Tcl_Obj* cv [2];
+
+	cv [0] = Tcl_NewStringObj ("::kinetcl::Valid",-1);
+	cv [1] = o;
+
+	if (Tcl_EvalObjv (interp, 2, cv, 0) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	c = kinetcl_context (interp);
+
+	*handle = c->mark;
+	c->mark = NULL;
+	return TCL_OK;
+    }
+}
+
+# # ## ### ##### ######## #############
+
+critcl::cproc ::kinetcl::start {Tcl_Interp* interp} ok {
+    XnStatus s = xnStartGeneratingAll (kinetcl_context (interp)->context);
+    CHECK_STATUS_RETURN;
+    return TCL_OK;
+}
+
+critcl::cproc ::kinetcl::stop {Tcl_Interp* interp} ok {
+    XnStatus s = xnStopGeneratingAll (kinetcl_context (interp)->context);
+    CHECK_STATUS_RETURN;
+    return TCL_OK;
+}
+
+# # ## ### ##### ######## #############
+
+critcl::cproc ::kinetcl::waitUpdate {Tcl_Interp* interp} ok {
+    XnStatus s = xnWaitAndUpdateAll (kinetcl_context (interp)->context);
+    CHECK_STATUS_RETURN;
+    return TCL_OK;
+}
+
+critcl::cproc ::kinetcl::waitAnyUpdate {Tcl_Interp* interp} ok {
+    XnStatus s = xnWaitAnyUpdateAll (kinetcl_context (interp)->context);
+    CHECK_STATUS_RETURN;
+    return TCL_OK;
+}
+
+critcl::cproc ::kinetcl::waitNoneUpdate {Tcl_Interp* interp} ok {
+    XnStatus s = xnWaitNoneUpdateAll (kinetcl_context (interp)->context);
+    CHECK_STATUS_RETURN;
+    return TCL_OK;
+}
+
+critcl::cproc ::kinetcl::waitOneUpdate {Tcl_Interp* interp Tcl_Obj* o} ok {
+    XnStatus s;
+    kinetcl_context_data c = kinetcl_context (interp);
+    XnNodeHandle handle;
+
+    if (kinetcl_validate (interp, o, &handle) != TCL_OK) {
+	return TCL_ERROR;
+    }
+
+    s = xnWaitOneUpdateAll (c->context, handle);
+    CHECK_STATUS_RETURN;
+    return TCL_OK;
+}
+
+# # ## ### ##### ######## #############
+
+critcl::cproc ::kinetcl::errorstate {Tcl_Interp* interp} ok {
+    XnStatus s = xnGetGlobalErrorState (kinetcl_context (interp)->context);
+    CHECK_STATUS_RETURN;
+    return TCL_OK;
+}
+
+# # ## ### ##### ######## #############
+
+critcl::ccommand ::kinetcl::mirror {} {  /* Syntax: mirror ?bool? */
+    kinetcl_context_data c = kinetcl_context (interp);
+
+    if (objc == 1) { /* Syntax: mirror */
+	XnBool m = xnGetGlobalMirror (c->context);
+
+	Tcl_SetObjResult (interp, Tcl_NewIntObj (m));
+	return TCL_OK;
+    }
+
+    if (objc == 2) { /* Syntax: mirror bool */
+	XnStatus s;
+	int      m;
+
+	if (Tcl_GetBooleanFromObj (interp, objv [1], &m) != TCL_OK) {
+	    return TCL_ERROR;
+	}
+
+	s = xnSetGlobalMirror (c->context, m);
+	CHECK_STATUS_RETURN;
+
+	return TCL_OK;
+    }
+
+    Tcl_WrongNumArgs (interp, 1, objv, "?bool?");
+    return TCL_ERROR;
+}
+
+# # ## ### ##### ######## #############
+## Need a way for the kt_callback to operate without a class around it.
+if 0 {
+XnStatus xnRegisterToGlobalErrorStateChange     (XnContext *pContext, XnErrorStateChangedHandler handler, void *pCookie, XnCallbackHandle *phCallback)
+void 	 xnUnregisterFromGlobalErrorStateChange (XnContext *pContext, XnCallbackHandle hCallback)
+}
+# # ## ### ##### ######## #############
+return
