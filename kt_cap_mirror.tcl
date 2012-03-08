e02b61e89b04f7123d77fa01ab6473d739cf6d76
Andreas Kupries <akupries@shaw.ca>
2012-03-08 05:00:57 +0000

    Modified the mirror capability, merged the two accessor methods into one.

--- kt_cap_mirror.tcl
+++ kt_cap_mirror.tcl
@@ -7,37 +7,30 @@ critcl::class def ::kinetcl::CapMirror {
 
     # # ## ### ##### ######## #############
 
-    mdef mirror { /* Syntax: <instance> mirror <bool> */
-	int m;
-	XnStatus s;
+    mdef mirror { /* Syntax: <instance> mirror ?<bool>? */
+	if (objc == 2) { /* Syntax: <instance> mirror */
+	    XnBool m = xnIsMirrored (instance->handle);
 
-	if (objc != 3) {
-	    Tcl_WrongNumArgs (interp, 2, objv, "bool");
-	    return TCL_ERROR;
+	    Tcl_SetObjResult (interp, Tcl_NewIntObj (m));
+	    return TCL_OK;
 	}
 
-	if (Tcl_GetBooleanFromObj (interp, objv[2], &m) != TCL_OK) {
-	    return TCL_ERROR;
+	if (objc == 3) { /* Syntax: <instance> mirror bool */
+	    XnStatus s;
+	    int m;
+
+	    if (Tcl_GetBooleanFromObj (interp, objv [2], &m) != TCL_OK) {
+		return TCL_ERROR;
+	    }
+
+	    s = xnSetMirror (instance->handle, m);
+	    CHECK_STATUS_RETURN;
+
+	    return TCL_OK;
 	}
 
-	s = xnSetMirror (instance->handle, m);
-	CHECK_STATUS_RETURN;
-
-	return TCL_OK;
-    }
-
-    mdef mirrored { /* Syntax: <instance> mirrored */
-	XnBool m;
-
-	if (objc != 2) {
-	    Tcl_WrongNumArgs (interp, 2, objv, NULL);
-	    return TCL_ERROR;
-	}
-
-	m = xnIsMirrored (instance->handle);
-
-	Tcl_SetObjResult (interp, Tcl_NewIntObj (m));
-	return TCL_OK;
+	Tcl_WrongNumArgs (interp, 2, objv, "?bool?");
+	return TCL_ERROR;
     }
 
     # # ## ### ##### ######## #############
