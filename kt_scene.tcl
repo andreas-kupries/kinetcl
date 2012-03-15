4ecee4afeae87b99709f92421abb45de35e8aeab
Andreas Kupries <akupries@shaw.ca>
2012-03-15 04:13:50 +0000

    Extended scene-analyzer with meta data retrieval method

--- kt_scene.tcl
+++ kt_scene.tcl
@@ -14,6 +14,23 @@ critcl::class def ::kinetcl::Scene {
     }
 
     # # ## ### ##### ######## #############
+
+    # # ## ### ##### ######## #############
+
+    support {
+	static Tcl_Obj*
+	kinetcl_convert_scene_metadata (XnSceneMetaData* meta)
+	{
+	    Tcl_Obj* res = Tcl_NewDictObj ();
+
+	    /* Only image specific information is pData, the raw bytes */
+	    Tcl_DictObjPut (NULL, res,
+			    Tcl_NewStringObj ("map",-1),
+			    kinetcl_convert_map_metadata (meta->pMap));
+	    return res;
+	}
+    }
+
     # # ## ### ##### ######## #############
 }
 
