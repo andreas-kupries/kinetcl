33dee06043aea591bd3ee237f15bdf4995533754
Andreas Kupries <akupries@shaw.ca>
2012-07-27 04:44:03 +0000

    Chasing compiler warnings, found and fixed genuine bug in kinetcl::user (pixelsof). Released scene metadata with the wrong functions. This was a memory nug waiting to happen. Affected examples are record-users and view-users, as the two users of the method in question.

--- kt_user.tcl
+++ kt_user.tcl
@@ -107,7 +107,7 @@ critcl::class def ::kinetcl::User {
 	s = xnGetUserPixels (instance->handle, id, meta);
 	/* CHECK_STATUS_RETURN, inlined, to free the meta data */
 	if (s != XN_STATUS_OK) {
-	    xnFreeImageMetaData (meta);
+	    xnFreeSceneMetaData (meta);
 	    Tcl_AppendResult (interp, xnGetStatusString (s), NULL);
 	    return TCL_ERROR;
 	}
@@ -128,7 +128,7 @@ critcl::class def ::kinetcl::User {
 	memcpy (image->pixel, meta->pData,
 		meta->pMap->pOutput->nDataSize);
 
-	xnFreeDepthMetaData (meta);
+	xnFreeSceneMetaData (meta);
 
 	Tcl_SetObjResult (interp, crimp_new_image_obj (image));
 	return TCL_OK;
