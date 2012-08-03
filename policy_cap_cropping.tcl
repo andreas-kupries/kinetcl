dd645b4e6635f89481ccde37091601842a90135b
Andreas Kupries <akupries@shaw.ca>
2012-08-03 18:20:15 +0000

    New wrapper class for cropping capability, allowing for simplification of code in C-level (command -> proc).

---
+++ policy_cap_cropping.tcl
@@ -0,0 +1,49 @@
+## -*- tcl -*-
+# # ## ### ##### ######## #############
+
+## This file defines a TclOO class 'CapCropping' wrapping around
+## the C level class 'CapCroppingC'.
+
+# # ## ### ##### ######## #############
+
+package require TclOO
+
+# # ## ### ##### ######## #############
+
+namespace eval ::kinetcl {}
+
+# # ## ### ##### ######## #############
+
+oo::class create ::kinetcl::CapCropping {
+    constructor {} {
+	::kinetcl::CapCroppingC create CAP
+	return
+    }
+
+    # # ## ### ##### ######## #############
+    ## Tcl wrapper for argument handling.
+
+    method crop {{x {}} {y {}} {w {}} {h {}}} {
+	set n [llength [info level 0]]
+	if {$n == 6} {
+	    CAP @crop: $x $y $w $h
+	} elseif {$n != 2} {
+	    return -code error "wrong\#args: ?x y w h?"
+	}
+	CAP @crop?
+    }
+
+    # # ## ### ##### ######## #############
+
+    # Expose the basic methods, and a few of the normally excluded
+    # special methods. Capability wrapper classes should pretty much
+    # look like their C class.
+
+    kinetcl::Publish ::kinetcl::CapCroppingC CAP
+    forward methods CAP methods
+    forward @self   CAP @self
+
+    # # ## ### ##### ######## #############
+}
+
+# # ## ### ##### ######## #############
